57da6f89ffcdf3409714db3058aac6cd000534ac
Mohammad Fuhad Uddin <mohammedfuhad1@gmail.com>
2021-11-11 02:46:26 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-/*********************************************************************************
- *  WEB422 – Assignment 1
- *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
- *  No part of this assignment has been copied manually or electronically from any other source
- *  (including web sites) or distributed to other students.
- *
- *  Name: Mohammad Fuhad Uddin Student ID: 135341196 Date: 18/09/21
- *  Heroku Link: _______________________________________________________________
- *
- ********************************************************************************/
+
 
 const express = require("express");
 const cors = require("cors");
